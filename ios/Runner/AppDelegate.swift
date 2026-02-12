b8ab5b16702c4e6eb32aef4173e5e485deb6af00
knuemeal16486 <knuemeal16486@gmail.com>
2026-02-12 18:34:15 +0900

    권한 재확인

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import flutter_local_notifications // [추가 1]
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,17 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // [추가 2] 포그라운드에서도 알림 배너 표시 설정
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+
+    // [추가 3] iOS 10 이상에서 알림 센터 대리자 설정 (필요시)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
