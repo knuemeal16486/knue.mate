03efeb892c8bf8bddc7c01ab12effdfaf95f1793
knuemeal16486 <knuemeal16486@gmail.com>
2026-02-21 20:32:34 +0900

    fix(campus_map): prevent MapController exception during state initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:home_widget/home_widget.dart';
